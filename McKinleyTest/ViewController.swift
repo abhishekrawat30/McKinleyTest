f64a9b032d611b8d8a06245d01344f92e1a2156f
Abhishek <abhishek.rawat@evontech.com>
2019-12-19 14:45:00 +0530

    Initial Commit

---
+++ McKinleyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  McKinleyTest
+//
+//  Created by Abhishek on 19/12/19.
+//  Copyright © 2019 Evontech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
