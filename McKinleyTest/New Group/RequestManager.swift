c7ab9527a2e2793f5792f2356e5974a329283b73
Abhishek <abhishek.rawat@evontech.com>
2019-12-19 16:26:07 +0530

    first commit

---
+++ McKinleyTest/New Group/RequestManager.swift
@@ -0,0 +1,9 @@
+//
+//  RequestManager.swift
+//  McKinleyTest
+//
+//  Created by Abhishek on 19/12/19.
+//  Copyright © 2019 Evontech. All rights reserved.
+//
+
+import Foundation
--- McKinleyTest/SingleTon/AppDelegate.swift
+++ McKinleyTest/SingleTon/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if UserDefaults().string(forKey: "token") != nil{
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: WebViewController.storyboardID)
+            vc.modalTransitionStyle = .crossDissolve
+            window?.rootViewController = vc
+            window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
